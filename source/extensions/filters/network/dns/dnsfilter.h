464dc3930fbb769ecc26c191778d96b361cadc3d
letrangeTiger <letranger1993@berkeley.edu>
2018-10-16 12:59:31 -0700

    Update dnsfilter.h

--- source/extensions/filters/network/dns/dnsfilter.h
+++ source/extensions/filters/network/dns/dnsfilter.h
@@ -1,1 +1,31 @@
-d
+#pragma once
+
+#include "envoy/network/filter.h"
+
+#include "common/common/logger.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace NetworkFilters {
+namespace DNSFilter {
+
+/**
+
+ */
+class DnsFilter : public Network::ReadFilter, Logger::Loggable<Logger::Id::filter> {
+public:
+  // Network::ReadFilter
+  Network::FilterStatus onData(Buffer::Instance& data, bool end_stream) override;
+  Network::FilterStatus onNewConnection() override { return Network::FilterStatus::Continue; }
+  void initializeReadFilterCallbacks(Network::ReadFilterCallbacks& callbacks) override {
+    read_callbacks_ = &callbacks;
+  }
+
+private:
+  Network::ReadFilterCallbacks* read_callbacks_{};
+};
+
+} // namespace DNSFilter
+} // namespace NetworkFilters
+} // namespace Extensions
+} // namespace Envoy
